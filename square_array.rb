b6f9cd7d74361992474553fb717797b6b4c1fd82
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-11 17:34:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,14 @@
+
+=begin
 def square_array(array)
   squared_array = []
   array.each{|element| squared_array << element ** 2}
+  squared_array
+end
+=end
+
+def square_array(array)
+  squared_array = []
+  array.each{|element| array[array.index(element)] = element ** 2}
   squared_array
 end
