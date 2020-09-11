1777fccd6af2c7c09332abccf86ba7ada9c1fd8f
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-11 17:38:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,9 +7,11 @@ def square_array(array)
 end
 =end
 
+=begin
 def square_array(array)
   array.each{|element| array[array.index(element)] = element ** 2}
 end
+=end
 
 def square_array(array)
   array.collect{|element| element ** 2}
